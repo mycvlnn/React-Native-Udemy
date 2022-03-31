1a2e64fbf1068fd1a0c45e66efcb73e76ba5e7bf
le ngoai ngu <lengoaingu@gmail.com>
2022-03-31 23:51:34 +0700

    react native handle event

--- App.js
+++ App.js
@@ -1,15 +1,50 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [listGoals, setListGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    console.log(enteredText);
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalSubmit = () => {
+    console.log("enteredGoal", enteredGoal);
+    const updatedListGoals = [...listGoals];
+    updatedListGoals.push(enteredGoal);
+    setListGoals(updatedListGoals);
+  };
+
+  const renderListGoals = () => {
+    return (
+      <View>
+        {listGoals.map((goal, index) => {
+          return (
+            <Text style={{ color: "red" }} key={index}>
+              {goal}
+            </Text>
+          );
+        })}
+      </View>
+    );
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.inputText} placeholder="Your course goal !" />
-        <Button title="Add Goal" />
+        <TextInput
+          style={styles.inputText}
+          placeholder="Your course goal !"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalSubmit} />
       </View>
       <View style={styles.listGoals}>
         <Text>List of goals...</Text>
+        {renderListGoals()}
       </View>
     </View>
   );
