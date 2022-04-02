74caf944a0156d0f4398c88f604ccffafa70370f
le ngoai ngu <lengoaingu@gmail.com>
2022-04-02 20:20:43 +0700

    46. Setup our screen components

---
+++ screens/StartGameScreen.jsx
@@ -0,0 +1,12 @@
+import { View, TextInput, Button } from "react-native";
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+      <Button title="Button" />
+    </View>
+  );
+};
+
+export default StartGameScreen;
