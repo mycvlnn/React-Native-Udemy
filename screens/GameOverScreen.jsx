608911ab9a70130d62115d981c7f4a427dd7eac6
le ngoai ngu <lengoaingu@gmail.com>
2022-04-05 22:32:46 +0700

    using styling nested text

--- screens/GameOverScreen.jsx
+++ screens/GameOverScreen.jsx
@@ -2,6 +2,11 @@ import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import Title from '../components/Title'
 import Colors from '../constants/colors'
+import { fontFamily } from '../constants/fonts'
+
+const Highlighter = ({ children }) => {
+  return <Text style={styles.textHighlight}>{children}</Text>
+}
 
 const GameOverScreen = () => {
   return (
@@ -10,6 +15,10 @@ const GameOverScreen = () => {
       <View style={styles.imageContainer}>
         <Image style={styles.image} source={require('../assets/success.png')} />
       </View>
+      <Text style={styles.summaryText}>
+        Your phone needed <Highlighter>X</Highlighter> rounds to guess the
+        number <Highlighter>Y</Highlighter>
+      </Text>
     </View>
   )
 }
@@ -18,11 +27,10 @@ export default GameOverScreen
 
 const styles = StyleSheet.create({
   rootContainer: {
-    padding: 30,
     justifyContent: 'center',
     alignItems: 'center',
     padding: 24,
-    marginTop: '20%'
+    marginTop: '10%'
   },
   imageContainer: {
     width: 300,
@@ -36,5 +44,17 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: '100%'
+  },
+  summaryText: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: Colors.primary800,
+    textShadowColor: Colors.accent500,
+    fontFamily: fontFamily.open_sans
+  },
+  textHighlight: {
+    fontWeight: 'bold',
+    fontFamily: fontFamily.open_sans_bold,
+    color: Colors.primary500
   }
 })
