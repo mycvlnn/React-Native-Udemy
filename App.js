8ba90f55fdf36ca031809e2823d264db71e8818f
le ngoai ngu <lengoaingu@gmail.com>
2022-04-14 23:22:28 +0700

    adding drawer navigation

--- App.js
+++ App.js
@@ -1,10 +1,74 @@
 import { Text, StyleSheet, View } from 'react-native'
+import { createDrawerNavigator, DrawerItemList } from '@react-navigation/drawer'
+import UserScreen from './screens/UserScreen'
+import WelcomeScreen from './screens/WelcomeScreen'
+import { NavigationContainer } from '@react-navigation/native'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import HomeScreen from './screens/HomeScreen'
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable'
+
+const Drawer = createDrawerNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Todo...</Text>
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="HomeScreen"
+        screenOptions={{
+          drawerLabel: 'Welcome',
+          drawerIcon: () => <Ionicons name="person" size={20} color="red" />,
+          drawerActiveBackgroundColor: 'orange',
+          headerShown: true,
+          title: 'Hahaha',
+          headerStyle: { backgroundColor: '#11bd28' },
+          headerTintColor: 'white',
+          headerRight: () => (
+            <Pressable onPress={() => alert('1')}>
+              <Ionicons
+                style={{ marginRight: 20 }}
+                name="person"
+                size={20}
+                color="white"
+              />
+            </Pressable>
+          ),
+          drawerLabelStyle: { color: 'black' },
+          drawerActiveTintColor: '#345'
+        }}
+      >
+        <Drawer.Screen
+          name="UserScreen"
+          component={UserScreen}
+          options={{ drawerLabel: 'Profile' }}
+        />
+        <Drawer.Screen
+          name="WelcomeScreen"
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: 'Welcome',
+            drawerIcon: () => <Ionicons name="person" size={20} color="red" />,
+            drawerActiveBackgroundColor: 'orange',
+            headerShown: true,
+            title: 'Hahaha',
+            headerStyle: { backgroundColor: '#11bd28' },
+            headerTintColor: 'white',
+            headerRight: () => (
+              <Pressable onPress={() => alert('1')}>
+                <Ionicons
+                  style={{ marginRight: 20 }}
+                  name="person"
+                  size={20}
+                  color="white"
+                />
+              </Pressable>
+            ),
+            drawerLabelStyle: { color: 'black' },
+            drawerActiveTintColor: '#345'
+          }}
+        />
+        <Drawer.Screen name="HomeScreen" component={HomeScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   )
 }
 
---
+++ screens/HomeScreen.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
+import { useDrawerStatus } from '@react-navigation/drawer'
+
+const HomeScreen = ({ navigation }) => {
+  const isDrawerOpen = useDrawerStatus() === 'open'
+
+  const toggleDrawerHandler = () => {
+    navigation.toggleDrawer()
+  }
+
+  const openDrawer = () => {
+    navigation.openDrawer()
+  }
+  const closeDrawer = () => {
+    navigation.closeDrawer()
+  }
+
+  return (
+    <View>
+      <Text style={styles.textDrawer}>Home Screen</Text>
+      <Text style={styles.textDrawer}>{isDrawerOpen ? 'True' : 'false'}</Text>
+      <Button onPress={toggleDrawerHandler} title="Toggle Drawer" />
+      <Button onPress={closeDrawer} title="Close Drawer" />
+      <Button onPress={openDrawer} title="Open Drawer" />
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  textDrawer: {
+    textAlign: 'center'
+  }
+})
